30b574985280ffd17cfb07fec739c2c2261b84ad
blayzeon <blayzeon@gmail.com>
2022-06-24 22:21:59 -0400

    change font and colors for accessibility

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,17 @@ import "./Card.css";
 import React from "react";
 
 export default function Card(props) {
-  const suitClass = `card-${props.suit[1]}`;
+  const suitClass = `card-${props.suit[1]} card-container`;
   return (
-    <div className="card-container" data-key={props.id} onClick={props.event}>
+    <div className={suitClass} data-key={props.id} onClick={props.event}>
       <div className="card-pair card-left">
         <p>{props.value}</p>
-        <div className={suitClass}>{props.suit[0]}</div>
+        <div>{props.suit[0]}</div>
       </div>
       <h1 className="card-center large-screen">{props.value}</h1>
       <div className="card-pair card-right upsidedown large-screen">
         <p>{props.value}</p>
-        <div className={suitClass}>{props.suit[0]}</div>
+        <div>{props.suit[0]}</div>
       </div>
     </div>
   );
