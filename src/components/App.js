46863eba44d00e0c4a98d6c9a0d85efdf8d91efa
blayzeon <blayzeon@gmail.com>
2022-06-24 20:10:35 -0400

    move functions to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Deck from "./Deck";
 import Popup from "./Popup";
 
 function App() {
+  /*
+          ♤ ♡ ♧ ♢
+    score & popup management
+          ♤ ♡ ♧ ♢
+  */
+
   const [score, setScore] = useState(0);
   const [bestScore, setBest] = useState(0);
   const [popupMessage, setMessage] = useState(false);
-  const DECK_SIZE = 52;
 
   const incrScore = () => {
     const currentScore = score + 1;
@@ -41,6 +46,116 @@ function App() {
     setMessage(message);
   }
 
+  /*
+          ♤ ♡ ♧ ♢
+    Create & manage the deck
+          ♤ ♡ ♧ ♢
+  */
+
+  const [cards, setCards] = useState([]);
+  const [cutCards, setCut] = useState([]);
+  const DECK_SIZE = 52;
+  const CARD_LIMIT = 12;
+
+  function shuffle(array) {
+    let newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i -= 1) {
+      const random = Math.floor(Math.random() * i);
+      const temp = newArray[i];
+      newArray[i] = newArray[random];
+      newArray[random] = temp;
+    }
+
+    return newArray;
+  }
+
+  function shuffleAndCut(array = cards) {
+    let deck = array;
+    if (deck.length < 1) {
+      return deck;
+    }
+
+    deck = shuffle(deck);
+    deck = deck.slice(0, CARD_LIMIT);
+
+    const unclicked = deck.find((card) => card.clicked === false);
+    if (!unclicked) {
+      console.log(true);
+      shuffleAndCut(array);
+      return;
+    }
+
+    return deck;
+  }
+
+  function setObjKey(array, key, value) {
+    let result = array;
+    result.forEach((object) => (object[key] = value));
+    return result;
+  }
+
+  const handleClick = (e) => {
+    const cardList = [...cards];
+    const target = e.target;
+    const container = target.closest("[data-key]");
+    const key = container.getAttribute("data-key");
+
+    const match = cardList.find((card) => card.key === key);
+
+    if (match.clicked) {
+      // reset the game state
+      resetScore();
+      const resetDeck = setObjKey(cards, "clicked", false);
+      setCards([...resetDeck]);
+    } else {
+      match.clicked = true;
+      incrScore();
+    }
+
+    setCut(shuffleAndCut(cards));
+  };
+
+  useEffect(() => {
+    const newCards = [];
+    const suits = [
+      ["♤", "spades"],
+      ["♡", "hearts"],
+      ["♧", "clubs"],
+      ["♢", "diamonds"],
+    ];
+
+    let count = 1;
+    while (newCards.length < DECK_SIZE) {
+      let value = count;
+      if (value === 1) {
+        value = "A";
+      } else if (value === 11) {
+        value = "J";
+      } else if (value === 12) {
+        value = "Q";
+      } else if (value === 13) {
+        value = "K";
+      }
+
+      for (let i = 0; i < suits.length; i += 1) {
+        newCards.push({
+          suit: suits[i],
+          value,
+          key: `${value}${suits[i][1]}`,
+          clicked: false,
+        });
+      }
+
+      count += 1;
+    }
+
+    setCards(newCards);
+  }, []);
+
+  useEffect(() => {
+    setCut(shuffleAndCut(cards));
+  }, [cards]);
+
   return (
     <div className="container">
       <Popup message={popupMessage} function={setPopupMessage} />
@@ -59,7 +174,9 @@ function App() {
           </p>
         </div>
       </div>
-      <Deck score={incrScore} reset={resetScore} />
+      <div className="container screen-size">
+        <Deck deck={cutCards} handleClick={handleClick} />
+      </div>
     </div>
   );
 }
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,112 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Card from "./Card";
-import { v4 as uuidv4 } from "uuid";
 
 export default function Deck(props) {
-  const CARD_LIMIT = 12;
-  let cutDeck = [];
-  const [cards, setCards] = useState([]);
-
-  function shuffle(array) {
-    let newArray = [...array];
-    for (let i = newArray.length - 1; i > 0; i -= 1) {
-      const random = Math.floor(Math.random() * i);
-      const temp = newArray[i];
-      newArray[i] = newArray[random];
-      newArray[random] = temp;
-    }
-
-    return newArray;
-  }
-
-  function shuffleAndCut(array) {
-    let deck = array;
-    deck = shuffle(deck);
-    deck = deck.slice(0, CARD_LIMIT);
-    return deck;
-  }
-
-  function setObjKey(array, key, value) {
-    let result = array;
-    result.forEach((object) => (object[key] = value));
-    return result;
-  }
-
-  /*
-    Create the DECK
-    ♤ ♡ ♧ ♢
-  */
-
-  useEffect(() => {
-    const newCards = [];
-    const suits = [
-      ["♤", "spades"],
-      ["♡", "hearts"],
-      ["♧", "clubs"],
-      ["♢", "diamonds"],
-    ];
-
-    let count = 1;
-    while (newCards.length < 52) {
-      let value = count;
-      if (value === 1) {
-        value = "A";
-      } else if (value === 11) {
-        value = "J";
-      } else if (value === 12) {
-        value = "Q";
-      } else if (value === 13) {
-        value = "K";
-      }
-
-      for (let i = 0; i < suits.length; i += 1) {
-        newCards.push({
-          suit: suits[i],
-          value,
-          key: uuidv4(),
-          clicked: false,
-        });
-      }
-
-      count += 1;
-    }
-
-    setCards(newCards);
-  }, []);
-
-  const handleClick = (e) => {
-    const cardList = [...cards];
-
-    const target = e.target;
-    const container = target.closest("[data-key]");
-    const key = container.getAttribute("data-key");
-
-    const match = cardList.find((card) => card.key === key);
-
-    if (match.clicked) {
-      // reset the game state
-      props.reset();
-      const resetDeck = setObjKey(cards, "clicked", false);
-      setCards([...resetDeck]);
-    } else {
-      match.clicked = true;
-      props.score();
-    }
-
-    cutDeck = shuffleAndCut(cards);
-  };
-
-  cutDeck = shuffleAndCut(cards);
   return (
     <div className="grid container">
-      {cutDeck.map((card) => {
+      {props.deck.map((card) => {
         return (
           <Card
             suit={card.suit}
             value={card.value}
             key={card.key}
             id={card.key}
-            event={handleClick}
+            event={props.handleClick}
           />
         );
       })}
